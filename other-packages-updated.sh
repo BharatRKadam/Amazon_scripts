4bab9cff6b06d37066c01eb1ade562be5cff49bb
BharatRKadam <158236006+BharatRKadam@users.noreply.github.com>
2024-10-20 23:45:09 +0530

    Update other-packages-updated.sh

--- other-packages-updated.sh
+++ other-packages-updated.sh
@@ -4,7 +4,7 @@ sudo apt update
 #install terraform
 sudo apt-get install unzip wget -y
 sudo wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip
-sudo unzip terraform_1.7.5_linux_amd64.zip
+sudo unzip terraform_1.9.8_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
 #install Kubectl on Jenkins
